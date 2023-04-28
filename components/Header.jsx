d77a325373bf6d19ffa6a4b471c50443a91a7952
archit bajpai <archit208027@gmail.com>
2023-04-28 22:46:52 +0530

    fixed bugs and branch merging

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
-import link from 'next/link';
+import React, { useState, useEffect } from 'react';
+
+// import link from 'next/link';
 import Link from 'next/link';
-const categories=[{
-    name:"RTeact",slug:"React"
-},
-{
-    name:"RTeact",slug:"React"
-}]
+import { getCategories } from '../services';
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
   return (
-    <div className='container mx-auto px-10 mb-8'>
-      <div className='border-b w-full inline-block border-blue-400 py-8'>
-         <div className='md:float-left block'>
-            <Link href="/">
-                <span className='cursor-pinter font-bold text-3xl text-white'>
-                    GraphCMS
-                </span>
-            </Link>
-         </div>
-         <div className='hidden md:float-left md:contents'>
-         {categories.map((category, index) => (
-            <Link key={index} href={`/category/${category.slug}`}>
-            <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
+    <div className="container mx-auto px-10 mb-8">
+      <div className="border-b w-full inline-block border-blue-400 py-8">
+        <div className="md:float-left block">
+          <Link href="/">
+            <span className="cursor-pointer font-bold text-4xl text-white">Graph CMS</span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
           ))}
-         </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
