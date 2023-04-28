b46f5eceb0c365c41c93b607c86d6d5eea912382
Utkarsh Sharma <supremetrinity151@gmail.com>
2023-04-28 19:56:43 +0530

    Update index.js

--- services/index.js
+++ services/index.js
@@ -1,39 +1,40 @@
-import {requests, gql} from 'graphql-request';
+import { request, gql } from 'graphql-request';
 
-const graphqlAPI= process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
-export const getPosts = async () =>{
-    const query = gql`
-    query MyQuery{
-        postsConnection {
-            edges {
-              node {
-                author {
-                  bio
-                  id
-                  name
-                  photo {
-                    url
-                  }
-                }
-                createdAt
-                slug
-                title
-                excerpt
-                featuredImage {
-                  url
-                }
-                categories {
-                  name
-                  slug
-                }
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
               }
             }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
           }
+        }
+      }
     }
-    `
+  `;
 
-    const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query);
 
-    return result.postConnection.edges;
-}
+  return result.postsConnection.edges;
+};
